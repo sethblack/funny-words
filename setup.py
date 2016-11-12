dffdf165cbaaf5c5ddba89e596e7020b43ce4d97
Seth Black <sblack@sethserver.com>
2016-11-12 09:02:27 -0600

    bump to version 1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='funny-words',
-    version='1.5',
+    version='1.6',
     packages=['funny_words'],
     author='Seth Black',
     author_email='sblack@sethserver.com',
